2ccd83b753e6cfaaecfe3c06be127350ae51b19e
Duncan Doyle <Duncan.Doyle@gmail.com>
2018-08-07 19:04:59 -0400

    Upgraded to the GA release of PAM 7.0.2.

--- support/openshift/provision.ps1
+++ support/openshift/provision.ps1
@@ -249,7 +249,6 @@ Function Create-Application() {
   $argList = "new-app --template=rhpam70-authoring"`
       + " -p APPLICATION_NAME=""$ARG_DEMO""" `
       + " -p IMAGE_STREAM_NAMESPACE=""$IMAGE_STREAM_NAMESPACE""" `
-      + " -p IMAGE_STREAM_TAG=""1.0""" `
       + " -p KIE_ADMIN_USER=""$KIE_ADMIN_USER""" `
       + " -p KIE_ADMIN_PWD=""$KIE_ADMIN_PWD""" `
       + " -p KIE_SERVER_CONTROLLER_USER=""$KIE_SERVER_CONTROLLER_USER""" `
--- support/openshift/provision.sh
+++ support/openshift/provision.sh
@@ -282,7 +282,6 @@ function create_application() {
   oc new-app --template=rhpam70-authoring \
 			-p APPLICATION_NAME="$ARG_DEMO" \
 			-p IMAGE_STREAM_NAMESPACE="$IMAGE_STREAM_NAMESPACE" \
-			-p IMAGE_STREAM_TAG="1.0" \
 			-p KIE_ADMIN_USER="$KIE_ADMIN_USER" \
 			-p KIE_ADMIN_PWD="$KIE_ADMIN_PWD" \
 			-p KIE_SERVER_CONTROLLER_USER="$KIE_SERVER_CONTROLLER_USER" \
